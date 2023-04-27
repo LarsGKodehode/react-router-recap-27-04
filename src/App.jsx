9cea2b44427d49ba2a3cdd5e3498a12f721562ff
larsg <lars.gunnar@jobloop>
2023-04-27 13:03:35 +0200

    added layout to site

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import {
   HashRouter as Router,
   Routes,
   Route,
+  Outlet,
+  Link,
 } from 'react-router-dom'
 
 import { LandingPage } from './routes/LandingPage';
@@ -13,16 +15,41 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path='/' element={<LandingPage /> } />
+        <Route path='/' element={<Layout /> }>
+          <Route index element={<LandingPage />} />
 
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/projects' element={<ProjectsPage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='projects' element={<ProjectsPage />} />
+        </Route>
+
 
         <Route path='*' element={<h1>404 Not Found</h1> } />
 
       </Routes>
     </Router>
   );
+}
+
+function Layout() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" >Home</Link>
+              <Link to="/about" >About</Link>
+              <Link to="/projects" >Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
 }
 
 export default App;
