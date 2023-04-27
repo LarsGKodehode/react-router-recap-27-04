be9c0b29e22ea90e3bf0377782d58a347acf69c7
larsg <lars.gunnar@jobloop>
2023-04-27 12:55:41 +0200

    first pass on implementing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import { LandingPage } from './routes/LandingPage';
+import { AboutPage } from './routes/about/AboutPage';
+import { ProjectsPage } from './routes/projects/ProjectsPage'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <Routes>
+
+        <Route path='/' element={<LandingPage /> } />
+
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+
+        <Route path='*' element={<h1>404 Not Found</h1> } />
+
+      </Routes>
+    </Router>
   );
 }
 
